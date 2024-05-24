35c11529df4ecd011ecdc9a3f1d3ed56e790e8ef
wafa REBHI <wafarebhi2@gmail.com>
2024-05-24 16:56:55 +0200

    creation d'un compte

--- cypress/e2e/projet_cypress.cy.js
+++ cypress/e2e/projet_cypress.cy.js
@@ -11,7 +11,18 @@ describe('Site Magento', () => {
     cy.url().should('eq', "https://magento.softwaretestingboard.com/customer/account/create/");
   });
 
-  it("Create an account", () => {
+  it.only("Create an account", () => {
+    cy.visit("https://magento.softwaretestingboard.com/customer/account/create/");
+    cy.get("#firstname").type("Emma").should("have.value","Emma");
+cy.get("#lastname").type("Dupont").should("have.value","Dupont");
+cy.get("#email_address").type("EmmaDupont11@gmail.com").should("have.value","EmmaDupont11@gmail.com");
+cy.get("#password").type("Emma1234@").should("have.value","Emma1234@");
+cy.get("#password-confirmation").type("Emma1234@").should("have.value","Emma1234@");
+cy.get("#form-validate > .actions-toolbar > div.primary").click();
+cy.url().should("include","/customer/account/");
+cy.get(".message-success").should("be.visible").and ("contains.text","Thank you for registering with Main Website Store.");
+cy.get(".box-content").should("contains.text","Emma").and("contains.text","Dupont").and("contains.text","EmmaDupont11@gmail.com");
+
 
   });
 
